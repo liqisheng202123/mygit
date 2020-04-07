cc6df8caa91f2786cb2a6c4718344e607d9fc90c
LQS <424167502@qq.com>
2020-04-07 16:55:05 +0800

    ffff

---
+++ lqs-test/src/main/java/com/lqs/test/lqstest/LqsTestApplication.java
@@ -0,0 +1,13 @@
+package com.lqs.test.lqstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LqsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LqsTestApplication.class, args);
+	}
+
+}
---
+++ lqs-test/src/test/java/com/lqs/test/lqstest/LqsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lqs.test.lqstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LqsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
